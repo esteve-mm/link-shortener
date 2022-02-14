37493e5c821c68648848619d29f48522bd806330
Esteve Martín Máximo <esteve.martin@uvesolutions.com>
2022-02-14 09:48:31 +0100

    Added in-memory rate limitig

---
+++ src/main/java/org/shrtr/core/services/InMemoryRateLimiting.java
@@ -0,0 +1,61 @@
+package org.shrtr.core.services;
+
+import lombok.*;
+import org.shrtr.core.domain.entities.Link;
+import org.shrtr.core.domain.entities.User;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.UUID;
+
+@Primary
+@Service
+@RequiredArgsConstructor
+public class InMemoryRateLimiting implements RateLimiting {
+
+    HashMap<UUID, Pair> linksRequests;
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @RequiredArgsConstructor
+    private static class Pair {
+        private LocalDateTime windowStart;
+        private Long requests;
+    }
+
+    @Override
+    public boolean isEnabled(User user) {
+        return user.hasRedirectRateLimit();
+    }
+
+    @Override
+    public boolean limitExceeded(Link link, User user) {
+        boolean exceeded = false;
+
+        var linkRequests = linksRequests.get(link.getId());
+
+        if (linkRequests == null){
+            // first redirect ever
+            linksRequests.put(link.getId(), new Pair(LocalDateTime.now(), 1L));
+        }
+        else if (Duration.between(linkRequests.windowStart, LocalDateTime.now()).toMillis() > user.getMaxRequestsWindowMs()) {
+            // expired window
+            linkRequests.setRequests(1L);
+            linkRequests.setWindowStart( LocalDateTime.now());
+        }
+        else {
+            // current window
+            if (linkRequests.getRequests() >= user.getMaxRequests()){
+                exceeded = true;
+            }
+            linkRequests.setRequests(linkRequests.getRequests() + 1);
+        }
+
+        return exceeded;
+    }
+}
